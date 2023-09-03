86f1424da9177a17fb3ebec9223d37e21ed0686f
Ozan <ozan.cetin@bite.software>
2023-09-03 23:29:43 +0200

    binary search algorithm

---
+++ python/binary_search.py
@@ -0,0 +1,16 @@
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] < target:
+            left = mid + 1
+        elif arr[mid] > target:
+            right = mid - 1
+        else:
+            return mid
+    return -1
+
+index = binary_search([1, 2, 3, 4, 5], 5) # 2
+
+print(f"index of the target is {index}")
+
